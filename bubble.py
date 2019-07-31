862923a50f05551179ce8c49fed1b7d5b7c55638
Paulo Ricardo Bernardo Silva <paulobernardo06@gmail.com>
2019-07-31 17:21:18 -0300

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -13,13 +13,13 @@ def geraLista(tam):
 mpl.use('Agg')
   
 def bubble(vetor):
-  number_iteration = 0
+  number_swap = 0
   for t in range(len(vetor)):
     for i in range ((len(vetor)-1)):
         if vetor[i]>vetor[i+1]:
-            number_iteration = number_iteration + 1
+            number_swap = number_swap + 1
             vetor[i], vetor[i + 1] = vetor[i + 1], vetor[i]
-  return number_iteration
+  return number_swap
 
 
 def desenhaGrafico(x,y, file_name, label, xl = "Entradas", yl = "Saídas"):
@@ -33,12 +33,12 @@ def desenhaGrafico(x,y, file_name, label, xl = "Entradas", yl = "Saídas"):
    
 tam = [10000,20000,50000,100000]
 times = []
-numbers_iteration = []
+numbers_swap = []
   
 for i in range(4):
     lista = geraLista(tam[i])
     times.append(timeit.timeit("bubble({})".format(lista),setup="from __main__ import bubble",number=1)) 
-    numbers_iteration.append(bubble(lista))
+    numbers_swap.append(bubble(lista))
 
 desenhaGrafico(tam,times, "GraficoTempo.png", "Tempo")
-desenhaGrafico(tam, numbers_iteration, "GraficoNumeroIteracoes.png", "NumeroIteracao")
+desenhaGrafico(tam, numbers_swap, "GraficoNumeroIteracoes.png", "NumeroIteracao")
